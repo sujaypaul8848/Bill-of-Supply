7b621a5d7cfd77742bd9c602b909a119c4b33c1f
anandbaburajan <anandbaburajan@gmail.com>
2023-10-03 01:36:18 +0530

    chore: don't show processing_fee_billing by default on partner creation

--- kinara_lms/public/js/lending/loan_partner.js
+++ kinara_lms/public/js/lending/loan_partner.js
@@ -4,7 +4,7 @@ frappe.ui.form.on("Loan Partner", {
     },
 
     toggle_processing_fee_billing(frm) {
-        if(!frm.doc.shareables.some(s => s.shareable_type === "Processing fee")) {
+        if(!frm.doc.shareables || !frm.doc.shareables.some(s => s.shareable_type === "Processing fee")) {
 			frm.toggle_display("processing_fee_billing", 0);
 		} else {
             frm.toggle_display("processing_fee_billing", 1);
