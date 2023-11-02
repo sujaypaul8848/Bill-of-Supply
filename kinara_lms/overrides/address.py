338770f7f0475f45fafd227df08374577efdd86d
anandbaburajan <anandbaburajan@gmail.com>
2023-11-02 20:18:35 +0530

    chore: update gst details on address update

--- kinara_lms/overrides/address.py
+++ kinara_lms/overrides/address.py
@@ -1,10 +1,29 @@
+import json
+
 import frappe
+
+from india_compliance.gst_india.overrides.party import update_docs_with_previous_gstin, get_docs_with_previous_gstin
 
 
 def kinara_address_validate(doc, method=None):
 	if doc.state:
 		doc.state = frappe.db.get_value("Kinara State", doc.state, "system_state_name")
 	
-	customer = frappe.db.get_value("Customer", {"customer_primary_address": doc.name})
-	if customer:
-		frappe.db.set_value("Customer", customer, {"gstin": doc.gstin, "gst_category": doc.gst_category})
+	if frappe.flags.in_update_docs_with_previous_gstin:
+		return
+
+	frappe.flags.in_update_docs_with_previous_gstin = True
+
+	previous_gstin = (doc.get_doc_before_save() or {}).get("gstin")
+
+	if not previous_gstin or previous_gstin == doc.gstin:
+		return
+
+	docs_with_previous_gstin = get_docs_with_previous_gstin(
+		previous_gstin, doc.doctype, doc.name
+    )
+
+	if not docs_with_previous_gstin:
+		return
+
+	update_docs_with_previous_gstin(doc.gstin, doc.gst_category, json.dumps(docs_with_previous_gstin))
