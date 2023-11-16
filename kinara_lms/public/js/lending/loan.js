97e1a94452496fea01e338e023710bf0e2a69c74
tohidmalik002 <touhid@8848digital.com>
2023-11-16 13:35:48 +0530

    update 2

--- kinara_lms/public/js/lending/loan.js
+++ kinara_lms/public/js/lending/loan.js
@@ -9,28 +9,28 @@ function applyCustomerTypeFilter(frm){
     frm.set_query('applicant', () => {
         return {
             filters: [
-                ["customer_type", "!=", "Individual"]
+                ["customer_type", "!=", "Individual"],
             ]
         };
     });
-    frm.set_query('custom_applicant', () => {
+    frm.set_query('custom_individual_applicant', () => {
         return {
             filters: [
-                ["customer_type", "=", "Individual"]
+                ["customer_type", "=", "Individual"],
             ]
         };
     });
     frm.fields_dict['custom_co_applicants'].grid.get_field('co_applicant').get_query = function(doc, cdt, cdn) {
         return {
             filters: [
-                ["customer_type", "=", "Individual"]
+                ["customer_type", "=", "Individual"],
             ]
         };
     };
     frm.fields_dict['custom_guarantors'].grid.get_field('guarantors').get_query = function(doc, cdt, cdn) {
         return {
             filters: [
-                ["customer_type", "=", "Individual"]
+                ["customer_type", "=", "Individual"],
             ]
         };
     };
