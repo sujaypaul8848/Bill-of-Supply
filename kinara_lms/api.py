28b250a5776f1dfd462e89645af0d63ae0ead20c
tohidmalik002 <touhid@8848digital.com>
2023-11-21 17:34:39 +0530

    Custom Get max tax rate api added

--- kinara_lms/api.py
+++ kinara_lms/api.py
@@ -212,8 +212,5 @@ def get_max_tax_rate(**kwargs):
 		frappe.throw("No Item Tax Template Available")
 	first_item_tax_template = doc.taxes[0].item_tax_template
 	doc = frappe.get_doc("Item Tax Template", first_item_tax_template)
-	max_tax_rate = doc.taxes[0].tax_rate
-	for tax in doc.taxes:
-		if tax.tax_rate > max_tax_rate:
-			max_tax_rate = tax.tax_rate
+	max_tax_rate = max(tax.tax_rate for tax in doc.taxes)
 	return {"max_tax_rate" : max_tax_rate}
