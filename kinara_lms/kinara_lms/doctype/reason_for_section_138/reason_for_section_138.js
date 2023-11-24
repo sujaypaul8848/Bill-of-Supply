5bcf40b83f1f986743d9da599331f992a4e7153b
tohidmalik002 <touhid@8848digital.com>
2023-11-24 16:33:40 +0530

    new doctype from 61 to 94

---
+++ kinara_lms/kinara_lms/doctype/reason_for_section_138/reason_for_section_138.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Visage Holdings and Finance Private Limited) and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Reason For Section 138", {
+// 	refresh(frm) {
+
+// 	},
+// });
