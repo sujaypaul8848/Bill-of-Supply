f4a503dce3e8d28e6b7f89e0c21de4fe81ca9ce6
tohidmalik002 <touhid@8848digital.com>
2023-12-12 15:38:04 +0530

    update 1

--- kinara_lms/kinara_lms/doctype/loan_ach/loan_ach.js
+++ kinara_lms/kinara_lms/doctype/loan_ach/loan_ach.js
@@ -1,8 +1,28 @@
 // Copyright (c) 2023, Visage Holdings and Finance Private Limited) and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Loan ACH", {
-// 	refresh(frm) {
+frappe.ui.form.on("Loan ACH", {
+    thirty_year_end: function(frm) {
+        if (frm.doc.thirty_year_end == 1) {
+            if (frm.doc.ach_start_date != undefined && frm.doc.ach_start_date != null) {
+                var startDate = frappe.datetime.str_to_obj(frm.doc.ach_start_date);
+                var thirtyYearsLater = new Date(startDate.getFullYear() + 30, startDate.getMonth(), startDate.getDate());
+                frm.set_value('ach_end_date', thirtyYearsLater);
+            } else {
+                frm.set_value('thirty_year_end', 0);
+                frappe.msgprint("Please Select ACH Start Date First");
+            }
+        } else {
+            frm.set_value('ach_end_date', null);
+        }
+    },
+    ach_start_date: function(frm) {
+        if (frm.doc.ach_start_date == undefined && frm.doc.ach_start_date == null) {
+            if (frm.doc.ach_end_date != undefined && frm.doc.ach_end_date != null) {
+                frm.set_value('ach_end_date', null);
+                frm.set_value('thirty_year_end', 0);
 
-// 	},
-// });
+            }
+        }
+    }
+});
--- kinara_lms/kinara_lms/doctype/loan_ach/loan_ach.py
+++ kinara_lms/kinara_lms/doctype/loan_ach/loan_ach.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2023, Visage Holdings and Finance Private Limited) and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class LoanACH(Document):
-	pass
+	def validate(self):
+		if self.thirty_year_end == 1:
+			if self.ach_start_date is not None:
+				self.ach_end_date = frappe.utils.data.add_years(self.ach_start_date, 30)
+			else:
+				frappe.throw("Invalid ACH Start Date")
