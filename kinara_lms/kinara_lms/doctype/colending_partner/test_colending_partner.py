a3208f9e18343c3883050077e2800be62dc4a2c4
tohidmalik002 <touhid@8848digital.com>
2023-12-13 12:10:47 +0530

    field customization + api's

--- kinara_lms/kinara_lms/doctype/colending_partner/test_colending_partner.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, Visage Holdings and Finance Private Limited) and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestColendingPartner(FrappeTestCase):
-	pass
--- kinara_lms/kinara_mdms/doctype/distance_from_branch_office/distance_from_branch_office.py
+++ kinara_lms/kinara_mdms/doctype/distance_from_branch_office/distance_from_branch_office.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class DistancefromBranchOffice(Document):
+class DistanceFromBranchOffice(Document):
 	pass
