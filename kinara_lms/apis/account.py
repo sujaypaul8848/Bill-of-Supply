9ddf5c4b894c68485665e99a191ca642e19b5f6f
Abhishek <abhishek.p@8848digital.com>
2023-12-17 23:13:44 +0530

    Added bank account api

--- kinara_lms/apis/account.py
+++ kinara_lms/apis/account.py
@@ -9,3 +9,15 @@ def get_default_disbursement_account(**kwargs):
 		frappe.throw("No Default Disbursement Account Found")
 	doc = frappe.get_doc("Account", doc.default_disbursement_account)
 	return doc
+
+@frappe.whitelist()
+def bank_account():
+	data = frappe.db.sql("""
+		SELECT
+			bap.name as bank_account_purpose,
+			ba.name as bank_account
+		FROM `tabBank Account` ba
+		left JOIN `tabBank Account Purpose` bap
+		ON ba.name = bap.value
+	""", as_dict=1)
+	return data
