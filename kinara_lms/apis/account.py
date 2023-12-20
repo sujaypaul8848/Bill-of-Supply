18d64736c8f2c7f81429e84428ff9b95d4555c14
tohidmalik002 <touhid@8848digital.com>
2023-12-20 15:49:16 +0530

    bank account purpose customize + default disbursement account logic update

--- kinara_lms/apis/account.py
+++ kinara_lms/apis/account.py
@@ -7,7 +7,7 @@ def get_default_disbursement_account(**kwargs):
 	doc = frappe.get_doc("Company", kwargs["Company Name"])
 	if not doc.default_disbursement_account:
 		frappe.throw("No Default Disbursement Account Found")
-	doc = frappe.get_doc("Account", doc.default_disbursement_account)
+	doc = frappe.get_doc("Bank Account", doc.default_disbursement_account)
 	return doc
 
 @frappe.whitelist()
